5bcaffa6b353339db9e9b84481061b16784b0bee
Nelson-Dominici <nelsondominici777@gmail.com>
2023-05-01 18:31:00 -0300

    connecting with the MySQL DB

---
+++ app/DataBase/ConnectDB.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\DataBase;
+
+use app\Utils\AppException;
+use app\Config;
+
+class ConnectDB
+{
+	private static $connection;
+
+	public static function connect(){
+
+		if(!isset(self::$connection)){
+			try {
+				self::$connection = new \PDO(
+					"mysql:host=".$_ENV["DB_HOST"].";dbname=".$_ENV["DB_NAME"], $_ENV["DB_USER"], 
+					$_ENV["DB_PASSWORD"]);
+			
+			} catch (\PDOException $e) {
+				throw new AppException("Something Went Wrong", 500);
+			}
+
+		}
+			
+		return self::$connection;
+	}
+}
