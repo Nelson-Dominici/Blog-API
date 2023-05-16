c074ededa26ba6d23c645d5ab6775c982d523fa4
Nelson-Dominici <nelsondominici777@gmail.com>
2023-05-15 23:51:51 -0300

    Post Routes and Redirecting

--- app/Routes.php
+++ app/Routes.php
@@ -6,5 +6,10 @@ use app\Framework\App;
 return function(App $app): void
 {
 	$userAccountRouter = require_once("Entity/User/Routes/UserAccountRouter.php");
+	$postsRoutes = require_once("Entity/Contente/Routes/PostsRoutes.php");
+	
 	$userAccountRouter($app);
+	$postsRoutes($app);
+	
+	header("Location: /");
 };
