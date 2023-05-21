b683ec7b88dd37f9907f781dfac186cecbbd2460
Nelson-Dominici <nelsondominici777@gmail.com>
2023-05-20 21:25:31 -0300

    Middleware, changing and adding routes

--- app/Entity/Contente/Routes/PostsRoutes.php
+++ app/Entity/Contente/Routes/PostsRoutes.php
@@ -3,9 +3,20 @@
 namespace app\Entity\Contente\Routes;
 
 use app\Entity\Contente\Controllers\PostsController;
+use app\Utils\Middleware\CheckTokenCookie;
 use app\Framework\App;
 
 return function(App $app): void
 {
-	$app->get("/", [PostsController::class, "postsView"]);
+	$app->get("/home", [PostsController::class, "allPostsView"]);
+
+	$app->get("/addPost", 
+		[CheckTokenCookie::class, "check"],
+		[PostsController::class, "addPostView"]
+	);
+
+	$app->post("/addPost", 
+		[CheckTokenCookie::class, "check"],
+		[PostsController::class, "addPost"]
+	);
 };
