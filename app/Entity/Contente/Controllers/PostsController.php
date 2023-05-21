42544b69c61b82c7061e280329b5e928f905864b
Nelson-Dominici <nelsondominici777@gmail.com>
2023-05-20 21:27:26 -0300

    Add post services, and render view of add post

--- app/Entity/Contente/Controllers/PostsController.php
+++ app/Entity/Contente/Controllers/PostsController.php
@@ -5,16 +5,36 @@ namespace app\Entity\Contente\Controllers;
 use app\Framework\Modules\Route\Services\Request\GetReq as Request;
 use app\Framework\Modules\Route\Services\Response\GetRes as Response;
 
-use app\Entity\Contente\Services\RegisterService;
-use app\Entity\Contente\Services\LoginService;
+use app\Entity\Contente\Services\AddPostService;
 use app\Utils\RenderView;
 
 class PostsController
 {
 
-	public static function postsView(Request $req, Response $res){
+	public static function allPostsView(Request $req, Response $res){
 		
 		RenderView::view("Templates/Contente/posts/allPosts");
 	}
 
+	public static function addPostView(Request $req, Response $res){
+	
+		if($req->payload->adm){
+			RenderView::view("Templates/Contente/posts/addPost");
+		}else{
+			header("Location: /home");
+		}
+	}
+
+	public static function addPost(Request $req, Response $res){
+
+		if($req->payload->adm){
+			AddPostService::add($req->body);
+
+			$res->status(200)->sendJson([
+				"success" => true
+			]);
+		}else{
+			header("Location: /home");
+		}
+	}
 }
