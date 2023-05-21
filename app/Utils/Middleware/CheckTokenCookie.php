1dd67e1ad311df287e47813c418da9a41e114025
Nelson-Dominici <nelsondominici777@gmail.com>
2023-05-20 21:18:12 -0300

    Authenticating jwt token from cookies

---
+++ app/Utils/Middleware/CheckTokenCookie.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\Utils\Middleware;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class CheckTokenCookie
+{
+
+	public static function check(){
+
+		if(!empty($_COOKIE["token"])){
+
+			$token = $_COOKIE["token"];
+
+			try {
+				$payload = JWT::decode($token, new Key($_ENV["JWT_KEY"], "HS256"));
+				return ["payload" => $payload];
+
+			}catch (\Exception $e) {
+				header("Location: /home");
+			}
+		}
+	
+		header("Location: /home");
+	}
+}
