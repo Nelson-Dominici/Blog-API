0d43443ae9a7cafa2a96ac2023654bd1cbf734cc
Nelson-Dominici <nelsondominici777@gmail.com>
2023-05-28 18:04:25 -0300

    New services for postController

--- app/Entity/Contente/Controllers/PostsController.php
+++ app/Entity/Contente/Controllers/PostsController.php
@@ -5,36 +5,81 @@ namespace app\Entity\Contente\Controllers;
 use app\Framework\Modules\Route\Services\Request\GetReq as Request;
 use app\Framework\Modules\Route\Services\Response\GetRes as Response;
 
+use app\Entity\Contente\Services\SeePostService;
 use app\Entity\Contente\Services\AddPostService;
+use app\Entity\Contente\Services\GetPostsService;
+use app\Entity\Contente\Services\DeletePostService;
 use app\Utils\RenderView;
 
 class PostsController
 {
 
-	public static function allPostsView(Request $req, Response $res){
+	public static function home(Request $req, Response $res): void{
 		
-		RenderView::view("Templates/Contente/posts/allPosts");
+		RenderView::view("Templates/Contente/posts/home");
 	}
 
-	public static function addPostView(Request $req, Response $res){
+	public static function postAdm(Request $req, Response $res): void{
 	
-		if($req->payload->adm){
-			RenderView::view("Templates/Contente/posts/addPost");
+		if($req->middlewareData["payload"]->adm){
+
+			RenderView::view("Templates/Contente/posts/postAdm");
+			
 		}else{
 			header("Location: /home");
 		}
 	}
 
-	public static function addPost(Request $req, Response $res){
+	public static function addPost(Request $req, Response $res): void{
 
-		if($req->payload->adm){
+		if($req->middlewareData["payload"]->adm){
+		
 			AddPostService::add($req->body);
 
 			$res->status(200)->sendJson([
 				"success" => true
 			]);
+
 		}else{
 			header("Location: /home");
+		}
+	}
+
+	public static function deletePost(Request $req, Response $res): void{
+
+		if($req->middlewareData["payload"]->adm){
+
+			DeletePostService::delete($req->urlParams);
+
+			$res->status(200)->sendJson([
+				"success" => true
+			]);
+
+		}else{
+			header("Location: /home");
+		}
+	}
+
+	public static function getPosts(Request $req, Response $res): void{
+
+		$posts = GetPostsService::get($req->queryParams);
+
+		$res->status(200)->sendJson([
+			"success" => true,
+			"data" => $posts
+		]);
+
+	}
+
+	public static function seePost(Request $req, Response $res): void{
+
+		$postContente = SeePostService::execute($req->urlParams);
+
+		if(!$postContente){
+			header("Location: /home");
+
+		}else{
+			RenderView::view("Templates/Contente/posts/post", $postContente);	
 		}
 	}
 }
