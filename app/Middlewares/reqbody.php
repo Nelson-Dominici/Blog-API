e5feccd73ddf799a1dd395d3cd5e8efefca73f17
Nelson-Dominici <nelsondominici777@gmail.com>
2023-06-04 22:15:57 -0300

    Middleware to get the request body

---
+++ app/Middlewares/reqbody.php
@@ -0,0 +1,21 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+return function(Request $request, RequestHandler $handler): Response
+{
+    $contentType = $request->getHeaderLine("Content-Type");
+
+    if (strstr($contentType, "application/json")) {
+     
+        $contents = json_decode(file_get_contents("php://input"), true);
+     
+        if (json_last_error() === JSON_ERROR_NONE) {
+            $request = $request->withParsedBody($contents);
+        }
+    }
+
+    return $handler->handle($request);
+};
