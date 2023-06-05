b07c4d65d10871f3e8897fd89050e6439d14ad9b
Nelson-Dominici <nelsondominici777@gmail.com>
2023-06-04 22:01:44 -0300

    Slim framework, middleware, handling errors

--- bootstrap.php
+++ bootstrap.php
@@ -2,9 +2,14 @@
 
 require_once("./vendor/autoload.php");
 
-use app\Framework\App;
+$body = require_once("app/Middlewares/reqBody.php");
+$handleErros = require_once("app/Middlewares/handleErros.php");
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
- 
-$app = new App();
+
+$app = Slim\Factory\AppFactory::create();
+$app->addRoutingMiddleware();
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorMiddleware->setDefaultErrorHandler($handleErros);
