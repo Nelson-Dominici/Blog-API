4d0311e40c3906a971546953587b6dd4f5915290
Nelson-Dominici <nelsondominici777@gmail.com>
2023-06-05 14:32:46 -0300

    Changing resources to those of the slim framework

--- app/Entity/User/Controllers/UserAccountController.php
+++ app/Entity/User/Controllers/UserAccountController.php
@@ -2,42 +2,41 @@
 
 namespace app\Entity\User\Controllers;
 
-use app\Framework\Modules\Route\Services\Request\GetReq as Request;
-use app\Framework\Modules\Route\Services\Response\GetRes as Response;
-
-use app\Entity\User\Services\RegisterService;
-use app\Entity\User\Services\LoginService;
+use app\Utils\SendJson;
 use app\Utils\RenderView;
+use app\Entity\User\Services\UserAccountServices\LoginService;
+use app\Entity\User\Services\UserAccountServices\RegisterService;
 
 class UserAccountController
 {
 
-	public static function login(Request $req, Response $res){
-		
-		LoginService::execute($req->body);
-		
-		$res->status(200)->sendJson([
-			"success" => true
-		]);
+	public static function login($req, $res)
+	{
+
+		LoginService::execute($req);
+		return SendJson::send(["success" => true ]);
+
 	}
 
-	public static function register(Request $req, Response $res){
+	public static function register($req, $res)
+	{
 
-		RegisterService::execute($req->body);
-		
-		$res->status(200)->sendJson([
-			"success" => true
-		]);
+		RegisterService::execute($req);
+		return SendJson::send(["success" => true ]);
 	}
 
-	public static function loginView(Request $req, Response $res){
+	public static function loginView($req, $res)
+	{
 		
-		RenderView::view("Templates/user/userAccount/login");
+		RenderView::view("Templates/user/userAccount/form/login");
+		return $res->withStatus(200);
 	}
 
-	public static function registerView(Request $req, Response $res){
+	public static function registerView($req, $res)
+	{
 
-		RenderView::view("Templates/user/userAccount/register");
+		RenderView::view("Templates/user/userAccount/form/register");
+		return $res->withStatus(200);
 	}
 
 }
