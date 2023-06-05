88b8a11339be5531a09f8a7f7184f9658f2b1e68
Nelson-Dominici <nelsondominici777@gmail.com>
2023-06-05 14:16:53 -0300

    Stripping namespace, new routes, redirection

--- app/Routes.php
+++ app/Routes.php
@@ -1,15 +1,13 @@
 <?php
 
-namespace app;
-use app\Framework\App;
-
-return function(App $app): void
+return function($app): void
 {
 	$userAccountRouter = require_once("Entity/User/Routes/UserAccountRouter.php");
-	$postsRoutes = require_once("Entity/Contente/Routes/PostsRoutes.php");
+	$userPostRoutes = require_once("Entity/Contente/Routes/UserPostRoutes.php");
+	$admPostRouter = require_once("Entity/Contente/Routes/AdmPostRouter.php");
 	
 	$userAccountRouter($app);
-	$postsRoutes($app);
+	$userPostRoutes($app);
+	$admPostRouter($app);
 	
-	header("Location: /user/login");
 };
