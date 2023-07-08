e1f109fbcf0c3706aa25d45e8d799c0455456457
Nelson-Dominici <nelsondominici777@gmail.com>
2023-07-07 22:24:58 -0300

    Service to delete a comment on a post

---
+++ app/Modules/Post/Comment/Services/DeleteCommentService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\Modules\Post\Comment\Services;
+
+use app\Entitys\Comments;
+use app\Helpers\EntityManagerHelper;
+
+
+class DeleteCommentService
+{
+
+	public static function delete(array $args, string $userUuid): void
+	{
+		
+		$entityManager = EntityManagerHelper::getEntityManager();
+
+		$commentRepository = $entityManager->getRepository(Comments::class);
+
+		$comment = $commentRepository->findOneBy([
+		    "userUuid" => $userUuid,
+		    "commentUuid" => $args["commentUuid"]
+		]);
+
+		if ($comment) {
+
+		    $entityManager->remove($comment);
+		    $entityManager->flush();
+		    
+		}
+
+	}
+
+} 
