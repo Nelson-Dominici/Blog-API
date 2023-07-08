9dabe9e1bac5fe298878693e9e60fcff370bf5aa
Nelson-Dominici <nelsondominici777@gmail.com>
2023-07-07 22:31:24 -0300

    Renaming file, using Doctrine ORM

---
+++ app/Modules/User/Account/Services/RegisterUserService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\Modules\User\Account\Services;
+
+use Ramsey\Uuid\Uuid;
+use app\Entitys\Users;
+use app\Helpers\AppException;
+use app\Helpers\EntityManagerHelper;
+
+class RegisterUserService
+{
+
+	public static function registerUser(array $reqBody): void
+	{
+
+		$entityManager = EntityManagerHelper::getEntityManager();
+		$usersRepository = $entityManager->getRepository(Users::class);
+		
+		$user = $usersRepository->findOneBy([
+		    "email" => $reqBody["email"]
+		]);
+
+		if ($user) {
+			throw new AppException("Email already registered");
+		}
+
+		$user = new Users(Uuid::uuid4());
+
+		$user->setName($reqBody["name"]);
+		$user->setEmail($reqBody["email"]);
+		$user->setPassword($reqBody["password"]);
+
+		$entityManager->persist($user);
+		$entityManager->flush();
+
+	}
+	
+}
