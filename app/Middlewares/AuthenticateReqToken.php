3951fe5e8e839951e3eae11a4a8060cf63bf2794
Nelson-Dominici <nelsondominici777@gmail.com>
2023-07-07 22:35:34 -0300

    Middleware to check request token

---
+++ app/Middlewares/AuthenticateReqToken.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace app\Middlewares;
+
+use Firebase\JWT;
+use app\Entitys\Users;
+use app\Helpers\SendJson;
+use app\Helpers\EntityManagerHelper;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+class AuthenticateReqToken
+{
+
+    public function __invoke(Request $req, RequestHandler $handler): Response
+    {
+
+    	$headers = $req->getHeaders();
+
+    	$tokenPayload = $this->authenticateJWT($headers);
+
+		if (!$tokenPayload) {
+
+			return SendJson::send([
+			    "success" => false,
+				"data" => ["message" => "Invalid token"]
+			], 401);
+
+		}
+
+		$user = $this->checkUserExists($tokenPayload->userUuid);
+		
+		if (!$user) {
+			
+			return SendJson::send([
+			    "success" => false,
+				"data" => ["message" => "Invalid token"]
+			], 401);
+
+		}
+
+		$req = $req->withAttribute("payload", $tokenPayload);
+
+	  	return $handler->handle($req);
+
+    }
+
+    private function authenticateJWT(array $headers): false|object
+    {
+
+		if (!empty($headers["authorization"])) {
+
+			try {
+
+				$token = $headers["authorization"][0];
+				return JWT\JWT::decode($token, new JWT\Key($_ENV["JWT_KEY"], "HS256"));
+			
+			} catch (\Exception $e) {
+
+				return false;
+			}	
+
+		}
+
+		return false;
+    
+    }
+
+    private function checkUserExists(string $userUuid): ?object
+    {
+
+		$entityManager = EntityManagerHelper::getEntityManager();
+		$usersRepository = $entityManager->getRepository(Users::class);
+		
+		$user = $usersRepository->findOneBy([
+		    "userUuid" => $userUuid
+		]);
+
+		return $user;
+    
+    }
+
+}
