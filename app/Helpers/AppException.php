566fd21402627a82269624ea58d2873ed60e669c
Nelson-Dominici <nelsondominici777@gmail.com>
2023-07-07 22:34:23 -0300

    Helper for throwing an app exception

---
+++ app/Helpers/AppException.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\Helpers;
+
+class AppException extends \Exception 
+{
+
+	function __construct( 
+		protected string $appMessage, 
+		protected int $statusCode = 400 
+	){}
+
+	public function getAppMessage(): string
+	{
+		return $this->appMessage;
+	}
+
+	public function getStatusCode(): int
+	{
+		return $this->statusCode;
+	}
+
+}
