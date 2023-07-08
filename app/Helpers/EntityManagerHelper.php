f279fe5af6d269536a37ffd9240bacbeb0fa434e
Nelson-Dominici <nelsondominici777@gmail.com>
2023-07-07 22:23:40 -0300

    ORM EntityManagerHelper

---
+++ app/Helpers/EntityManagerHelper.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\Helpers;
+
+use Doctrine\ORM\ORMSetup;
+use Doctrine\ORM\EntityManager;
+use Doctrine\DBAL\DriverManager;
+
+class EntityManagerHelper
+{
+	
+	public static function getEntityManager(): EntityManager
+	{
+
+		$config = ORMSetup::createAttributeMetadataConfiguration(
+		   paths: array(__DIR__."/app"),
+		   isDevMode: true,
+		);
+
+		$connectionDB = DriverManager::getConnection([
+		    "dbname" => $_ENV["DB_NAME"],
+		    "user" => $_ENV["DB_USER"],
+		    "password" => $_ENV["DB_PASSWORD"],
+		    "host" => $_ENV["DB_HOST"],
+		    "driver" => "pdo_mysql",
+		]);
+
+        return new EntityManager($connectionDB, $config);
+
+	}
+
+}
