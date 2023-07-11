9a3cc5f494cbb06913ec9b776e6eb2c3e317b98d
Nelson-Dominici <nelsondominici777@gmail.com>
2023-07-10 23:09:26 -0300

    New routes, typing variable $group

--- app/Modules/Post/Comment/PostCommentRouter.php
+++ app/Modules/Post/Comment/PostCommentRouter.php
@@ -1,15 +1,17 @@
 <?php
 
 namespace app\Modules\Post\Comment;
+use Slim\Routing\RouteCollectorProxy;
 
 $group->get(
 	"/getComments/{postUuid}", [PostCommentController::class, "getComments"]
 );
 
-$group->group("", function($group)
+$group->group("", function(RouteCollectorProxy $group)
 {
 
-	$group->post("", [PostCommentController::class, "addComment"]);
+	$group->post("", [PostCommentController::class, "addPostComment"]);
+	$group->patch("/edit", [PostCommentController::class, "editPostComment"]);
 	
 	$group->delete(
 		"/delete/{commentUuid}", [PostCommentController::class, "deleteComment"]
