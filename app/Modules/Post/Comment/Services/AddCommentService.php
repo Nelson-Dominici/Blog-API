3eff98ec89eea69c7e55ca7be54fcbcff997908e
Nelson-Dominici <nelsondominici777@gmail.com>
2023-07-10 23:14:30 -0300

    Deleting comments from the post, after deleting it

--- app/Modules/Post/Comment/Services/AddCommentService.php
+++ app/Modules/Post/Comment/Services/AddCommentService.php
@@ -3,7 +3,6 @@
 namespace app\Modules\Post\Comment\Services;
 
 use Ramsey\Uuid\Uuid;
-use app\Entitys\Users;
 use app\Entitys\Posts;
 use app\Entitys\Comments;
 use app\Helpers\EntityManagerHelper;
@@ -15,23 +14,21 @@ class AddCommentService
 	{
 
 		$entityManager = EntityManagerHelper::getEntityManager();
-
-		$usersRepository = $entityManager->getRepository(Users::class);
 		$postsRepository = $entityManager->getRepository(Posts::class);
-
-		$commentUuid = Uuid::uuid4();
-
-		$comments = new Comments(new \DateTime(), $commentUuid);
-
-		$comments->setUserUuid($userUuid);
-		$comments->setPostUuid($reqBody["postUuid"]);
-		$comments->setContente($reqBody["contente"]);
 
 		$post = $postsRepository->findOneBy([
 		    "postUuid" => $reqBody["postUuid"]
 		]);
 
 		if ($post) {
+
+			$commentUuid = Uuid::uuid4();
+
+			$comments = new Comments(new \DateTime(), $commentUuid);
+
+			$comments->setUserUuid($userUuid);
+			$comments->setPostUuid($reqBody["postUuid"]);
+			$comments->setContente($reqBody["contente"]);
 
 			$entityManager->persist($comments);
 			$entityManager->flush();
