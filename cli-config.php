4cbaca4f5bfff3654bce42fbc3709c66ed75530e
Nelson-Dominici <nelsondominici777@gmail.com>
2023-07-11 18:21:15 -0300

    Configuring Doctrine Console Runner

---
+++ cli-config.php
@@ -0,0 +1,10 @@
+<?php
+
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use app\Helpers\EntityManagerHelper;
+
+require_once "bootstrap.php";
+
+$entityManager = EntityManagerHelper::getEntityManager();
+
+return ConsoleRunner::createHelperSet($entityManager);
