a2880c61247b6624d103f31f689da6f46b83cbc0
Nelson-Dominici <nelsondominici777@gmail.com>
2023-07-10 23:28:44 -0300

    Deleting comments after post is deleted

--- app/Modules/Post/PostAdm/Services/DeletePostService.php
+++ app/Modules/Post/PostAdm/Services/DeletePostService.php
@@ -3,6 +3,7 @@
 namespace app\Modules\Post\PostAdm\Services;
 
 use app\Entitys\Posts;
+use app\Entitys\Comments;
 use app\Helpers\AppException;
 use app\Helpers\EntityManagerHelper;
 
@@ -13,19 +14,29 @@ class DeletePostService
 	{
 
 		$entityManager = EntityManagerHelper::getEntityManager();
+
 		$postsRepository = $entityManager->getRepository(Posts::class);
+		$commentRepository = $entityManager->getRepository(Comments::class);
 
 		$post = $postsRepository->findOneBy([
 		    "postUuid" => $urlParams["postUuid"]
 		]);
 
 		if (!$post) {
-
+			
 			throw new AppException("Post uuid not found", 404);
+
 		}
 
     	$entityManager->remove($post);
 	    $entityManager->flush();
+
+		$queryBuilder = $commentRepository->createQueryBuilder("e")
+		->delete()
+	    ->where("e.postUuid = :postUuid")
+		->setParameter("postUuid", $urlParams["postUuid"]);
+
+		$queryBuilder->getQuery()->execute();
 
 	}
 
