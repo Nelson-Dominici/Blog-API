48d06f0b3725e0456016affad035446ddd0d33f3
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-12 10:45:17 -0300

    Throwing exception if user or post does not exist.

--- app/Modules/Comment/Services/AddCommentService.php
+++ app/Modules/Comment/Services/AddCommentService.php
@@ -2,38 +2,48 @@
 
 namespace app\Modules\Comment\Services;
 
+use app\Entitys\{
+	Posts,
+	Comments
+};
+
 use Ramsey\Uuid\Uuid;
-use app\Entitys\Posts;
-use app\Entitys\Comments;
-use app\Helpers\EntityManagerHelper;
+
+use app\Helpers\{
+	AppException,
+	EntityManagerHelper
+};
 
 class AddCommentService
 {
-
-	public static function add(array $reqBody, string $userUuid): void
+	public static function handle(array $reqBody, string $userUuid): void
 	{
-
 		$entityManager = EntityManagerHelper::getEntityManager();
+
+		$userRepository = $entityManager->getRepository(User::class);
 		$postsRepository = $entityManager->getRepository(Posts::class);
 
 		$post = $postsRepository->findOneBy([
 		    "uuid" => $reqBody["postUuid"]
 		]);
 
-		if ($post) {
+		$user = $userRepository->findOneBy([
+		    "uuid" => $userUuid
+		]);
 
-			$commentUuid = Uuid::uuid4();
-
-			$comments = new Comments(new \DateTime(), $commentUuid);
-
-			$comments->setUserUuid($userUuid);
-			$comments->setPostUuid($reqBody["postUuid"]);
-			$comments->setContente($reqBody["contente"]);
-
-			$entityManager->persist($comments);
-			$entityManager->flush();
+		if (!$post || !$user) {
+			throw new AppException("Post or user does not exist.");
 		}
 
-	}
+		$commentUuid = Uuid::uuid4();
 
+		$comments = new Comments(new \DateTime(), $commentUuid);
+
+		$comments->setUserUuid($userUuid);
+		$comments->setPostUuid($reqBody["postUuid"]);
+		$comments->setContente($reqBody["contente"]);
+
+		$entityManager->persist($comments);
+		$entityManager->flush();
+	}
 } 
