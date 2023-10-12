bbe1505ce83f3a8ce75abe27d02d4ec23134b5ac
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-12 17:17:58 -0300

    Adjusting code.

--- app/Modules/User/Router.php
+++ app/Modules/User/Router.php
@@ -7,8 +7,8 @@ use Slim\Routing\RouteCollectorProxy;
 $group->post("", [Controller::class, "store"]);
 $group->post("/login", [Controller::class, "login"]);
 
-$group->group("", function(RouteCollectorProxy $group): void
-{
+$group->group("", function(RouteCollectorProxy $group): void {
+
 	$group->patch("", [Controller::class, "update"]);
 	$group->delete("", [Controller::class, "destroy"]);
 	
