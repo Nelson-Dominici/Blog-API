bbb0418557365f2358b3ec2598d1d4d6a738ece1
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-12 19:32:38 -0300

    Placing administrator resources.

--- app/Modules/Post/Controller.php
+++ app/Modules/Post/Controller.php
@@ -15,6 +15,74 @@ class Controller
 {
 	use ApiResponseTrait;
 
+	public function editContente(Request $req, Response $res, array $args): Response
+	{
+		if ($req->getAttribute("payload")->adm) {
+
+			v::key(
+	    		"newContente", v::stringType()->notEmpty()	
+	    	)->assert($req->getParsedBody());
+
+			Services\EditPostContenteService::handle(
+				$args["postUuid"],
+				$req->getParsedBody()
+			);
+
+			return $this->success();
+		}
+        
+		return $this->error("Unauthorized user.", 401);
+	}
+	
+	public function editTitle(Request $req, Response $res, array $args): Response
+	{
+		if ($req->getAttribute("payload")->adm) {
+
+			v::key(
+	    		"newTitle", v::stringType()->notEmpty()->length(null, 100)
+	    	)->assert($req->getParsedBody());
+
+			Services\EditPostTitleService::handle(
+				$args["postUuid"],
+				$req->getParsedBody()
+			);
+
+			return $this->success();
+		}
+        
+		return $this->error("Unauthorized user.", 401);
+	}
+
+	public function store(Request $req, Response $res): Response
+	{
+		if ($req->getAttribute("payload")->adm) {
+			
+			v::key(
+				"title", v::stringType()->notEmpty()->length(null, 100)
+			)->key(
+	    		"contente", v::stringType()->notEmpty()	
+	    	)->assert($req->getParsedBody());
+
+			Services\AddPostService::handle($req->getParsedBody());
+
+			return $this->success();
+		}
+        
+		return $this->error("Unauthorized user.", 401);
+	}
+
+	public function destroy(Request $req, Response $res, array $args): Response
+	{
+		if ($req->getAttribute("payload")->adm) {
+
+			Services\DeletePostService::handle($args["postUuid"]);
+
+			return $this->success();
+		}
+        
+		return $this->error("Unauthorized user.", 401);
+	}
+
 	public function index(Request $req, Response $res): Response
 	{
 		$posts = Services\GetPostsService::handle($req->getQueryParams());
