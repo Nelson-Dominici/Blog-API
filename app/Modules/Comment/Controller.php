8f3963ffdde97198d075a26199b8ad3e115db3c9
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-12 10:29:21 -0300

    Using the new way of send a response.

--- app/Modules/Comment/Controller.php
+++ app/Modules/Comment/Controller.php
@@ -2,74 +2,66 @@
 
 namespace app\Modules\Comment;
 
-use app\Helpers\SendJson;
+use app\Helpers\ApiResponseTrait;
 use Respect\Validation\Validator as v;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class Controller
 {
-	
+	use ApiResponseTrait;
+
 	public function store(Request $req, Response $res): Response
 	{
-
 		v::key(
 			"contente", v::stringType()->notEmpty()
 		)->key(
     		"postUuid", v::stringType()->notEmpty()
     	)->assert($req->getParsedBody());
 
-		$data = Services\AddCommentService::add(
+		Services\AddCommentService::handle(
 			$req->getParsedBody(),
 			$req->getAttribute("payload")->userUuid
 		);
 
-		return SendJson::send(["success" => true]);
-
+		return $this->success();
 	}
 
 	public function index(Request $req, Response $res, array $args): Response
 	{
-
-		$comments = Services\GetCommentsService::get(
+		$comments = Services\GetCommentsService::handle(
 			$args["postUuid"], 
 			$req->getQueryParams()
 		);
 
-		return SendJson::send([
-			"success" => true,
-			"data" => $comments
-		]);
-
+		return $this->success($comments);
 	}
 
 	public function destroy(Request $req, Response $res, array $args): Response
 	{
 
-		Services\DeleteCommentService::delete(
+		var_dump($req->getAttribute("payload"));
+		die();
+		Services\DeleteCommentService::handle(
 			$args, $req->getAttribute("payload")->userUuid
 		);
 
-		return SendJson::send(["success" => true]);
-		
+		return $this->success();
 	}
 
 	public function update(Request $req, Response $res): Response
 	{
-
 		v::key(
 			"newContente", v::stringType()->notEmpty()
 		)->key(
     		"commentUuid", v::stringType()->notEmpty()
     	)->assert($req->getParsedBody());
 
-		Services\EditPostCommentService::editComment(
+		Services\EditPostCommentService::handle(
 			$req->getParsedBody(), 
 			$req->getAttribute("payload")->userUuid
 		);
 
-		return SendJson::send(["success" => true]);
-		
+		return $this->success();
 	}
-
 }
