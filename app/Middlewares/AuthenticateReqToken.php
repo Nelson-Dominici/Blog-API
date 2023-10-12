1bf49eeabc2609ae8c0770be9d37ae7e86c05745
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-12 17:21:31 -0300

    Removing unnecessary checking.

--- app/Middlewares/AuthenticateReqToken.php
+++ app/Middlewares/AuthenticateReqToken.php
@@ -3,82 +3,53 @@
 namespace app\Middlewares;
 
 use Firebase\JWT;
-use app\Entitys\Users;
-use app\Helpers\SendJson;
-use app\Helpers\EntityManagerHelper;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+
+use Psr\Http\Message\{ 
+	ResponseInterface as Response,
+	ServerRequestInterface as Request
+};
+
+use app\Helpers\AppException;
+
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 class AuthenticateReqToken
 {
-
     public function __invoke(Request $req, RequestHandler $handler): Response
     {
-
     	$headers = $req->getHeaders();
 
-    	$tokenPayload = $this->authenticateJWT($headers);
-
-		if (!$tokenPayload) {
-
-			return SendJson::send([
-			    "success" => false,
-				"data" => ["message" => "Invalid token"]
-			], 401);
-
+		if (
+			!array_key_exists("Authorization", $headers) || 
+			!$headers["Authorization"][0]
+		) {
+			throw new AppException("Not Authorized.", 401);
 		}
 
-		$user = $this->checkUserExists($tokenPayload->userUuid);
-		
-		if (!$user) {
-			
-			return SendJson::send([
-			    "success" => false,
-				"data" => ["message" => "Invalid token"]
-			], 401);
+		$token = $headers["Authorization"][0];
 
+		$parts = explode(" ", $token);
+
+		if ($parts[0] != "Bearer" || empty($parts[1])) {
+
+			throw new AppException("Invalid token.", 401);
+		}
+
+		$tokenPayload = null;
+	
+		try {
+
+			$tokenPayload = JWT\JWT::decode(
+				$parts[1], 
+				new JWT\Key($_ENV["JWT_KEY"], "HS256"),
+			);
+
+		} catch (\Exception $e) {
+			throw new AppException("Invalid token.", 401);
 		}
 
 		$req = $req->withAttribute("payload", $tokenPayload);
 
 	  	return $handler->handle($req);
-
     }
-
-    private function authenticateJWT(array $headers): false|object
-    {
-
-		if (!empty($headers["authorization"])) {
-
-			try {
-
-				$token = $headers["authorization"][0];
-				return JWT\JWT::decode($token, new JWT\Key($_ENV["JWT_KEY"], "HS256"));
-			
-			} catch (\Exception $e) {
-
-				return false;
-			}	
-
-		}
-
-		return false;
-    
-    }
-
-    private function checkUserExists(string $userUuid): ?object
-    {
-
-		$entityManager = EntityManagerHelper::getEntityManager();
-		$usersRepository = $entityManager->getRepository(Users::class);
-		
-		$user = $usersRepository->findOneBy([
-		    "uuid" => $userUuid
-		]);
-
-		return $user;
-    
-    }
-
 }
