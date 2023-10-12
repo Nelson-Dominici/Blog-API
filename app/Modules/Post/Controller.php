ab56c5348de59b490e87a1b92ea6b1a44ced173a
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-11 22:05:46 -0300

    Using the new way of send a response.

--- app/Modules/Post/Controller.php
+++ app/Modules/Post/Controller.php
@@ -2,42 +2,32 @@
 
 namespace app\Modules\Post;
 
-use app\Helpers\SendJson;
+use app\Helpers\ApiResponseTrait;
 use Respect\Validation\Validator as v;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class Controller
 {
+	use ApiResponseTrait;
 
 	public function index(Request $req, Response $res): Response
 	{
-
 		$posts = Services\GetPostsService::get($req->getQueryParams());
 		
-		return SendJson::send([
-		    "success" => true,
-			"data" => $posts
-		]);
-		
+		return $this->success($posts);
 	}
 
 	public function show(Request $req, Response $res, array $args): Response
 	{
-
 		$data = Services\GetPostInfosService::get($args);
 
 		if ($data) {
 			
-			return SendJson::send([
-			    "success" => true,
-				"data" => $data
-			]);
+			return $this->success($data);
 			
 		}
 
-		return SendJson::send(["success" => false]);
-
+		return $this->error();
 	}
-	
 }
