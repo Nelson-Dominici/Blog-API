ab41d6e684bbbf27fb9db4246f024b4ad01faaca
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-12 18:43:50 -0300

    Transferring admin services to the poste folder.

---
+++ app/Modules/Post/Services/DeletePostService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\Modules\Post\Services;
+
+use app\Entitys\Posts;
+use app\Entitys\Comments;
+use app\Helpers\AppException;
+use app\Helpers\EntityManagerHelper;
+
+class DeletePostService
+{
+	
+	public static function delete(array $urlParams): void
+	{
+
+		$entityManager = EntityManagerHelper::getEntityManager();
+
+		$postsRepository = $entityManager->getRepository(Posts::class);
+		$commentRepository = $entityManager->getRepository(Comments::class);
+
+		$post = $postsRepository->findOneBy([
+		    "uuid" => $urlParams["postUuid"]
+		]);
+
+		if (!$post) {
+			
+			throw new AppException("Post uuid not found", 404);
+
+		}
+
+    	$entityManager->remove($post);
+	    $entityManager->flush();
+
+		$queryBuilder = $commentRepository->createQueryBuilder("e")
+		->delete()
+	    ->where("e.postUuid = :postUuid")
+		->setParameter("postUuid", $urlParams["postUuid"]);
+
+		$queryBuilder->getQuery()->execute();
+
+	}
+
+}
