82538a1738324c202b662e0ee7bb0ce935226036
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-13 12:03:18 -0300

    Adjust typing.

--- app/Routes.php
+++ app/Routes.php
@@ -1,14 +1,14 @@
 <?php
 
-use Slim\Routing\RouteCollectorProxy;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
-$app->group("/api", function(RouteCollectorProxy $group): void {
+$app->group("/api", function(Group $group): void {
 
-	$group->group("/v1/users", function(RouteCollectorProxy $group): void {
+	$group->group("/v1/users", function(Group $group): void {
 		require_once("Modules/User/UserRouter.php");
 	});
 
-	$group->group("/v1/posts", function(RouteCollectorProxy $group): void {
+	$group->group("/v1/posts", function(Group $group): void {
 		require_once("Modules/Post/PostRouter.php");
 		require_once("Modules/Comment/CommentRouter.php");
 	}); 
