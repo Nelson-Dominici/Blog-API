4b9224f2a863f554423af0198e98f72250fb9d2f
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-13 10:28:14 -0300

    Using new file names.

--- app/Routes.php
+++ app/Routes.php
@@ -5,12 +5,12 @@ use Slim\Routing\RouteCollectorProxy;
 $app->group("/api", function(RouteCollectorProxy $group): void {
 
 	$group->group("/v1/users", function(RouteCollectorProxy $group): void {
-		require_once("Modules/User/Router.php");
+		require_once("Modules/User/UserRouter.php");
 	});
 
 	$group->group("/v1/posts", function(RouteCollectorProxy $group): void {
-		require_once("Modules/Post/Router.php");
-		require_once("Modules/Comment/Router.php");
+		require_once("Modules/Post/PostRouter.php");
+		require_once("Modules/Comment/CommentRouter.php");
 	}); 
 
 });	
