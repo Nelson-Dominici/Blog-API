e87217e9e2d0a2865423b5d4318b53efc0d22729
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-14 18:32:21 -0300

    Returning admin name.

--- app/Modules/Post/Services/GetPostsService.php
+++ app/Modules/Post/Services/GetPostsService.php
@@ -2,7 +2,10 @@
 
 namespace app\Modules\Post\Services;
 
-use app\Entitys\Posts;
+use app\Entitys\{
+	Users,
+	Posts,
+};
 
 use app\Helpers\EntityManagerHelper;
 
@@ -16,11 +19,14 @@ class GetPostsService
 		$postsRepository = $entityManager->getRepository(Posts::class);
 
 		$posts = $postsRepository->createQueryBuilder("p")
-            ->select("p.title", "p.uuid")
+            ->select("p.title", "p.uuid", "u.name")
+            ->innerJoin(Users::class, "u", "WITH", "p.userUuid = u.uuid")
             ->orderBy("p.date", "DESC")
             ->setMaxResults(5)
-            ->setFirstResult($skipQuery);
+            ->setFirstResult($skipQuery)
+			->getQuery()
+			->getResult();
 
-		return $posts->getQuery()->getResult();
+		return $posts;
 	}
 }
