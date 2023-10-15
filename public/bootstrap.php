9c095c4b44132d5d905da0bf650d35f524523a8b
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-15 16:55:53 -0300

    Public folder.

---
+++ public/bootstrap.php
@@ -0,0 +1,14 @@
+<?php 
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv->load();
+
+$app = Slim\Factory\AppFactory::create();
+$app->addRoutingMiddleware();
+
+$app->add(new \app\Middlewares\GetReqbody());
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorMiddleware->setDefaultErrorHandler(new \app\Middlewares\HandleExceptions());
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once "bootstrap.php";
+
+require_once __DIR__ . "/../app/routes.php";
+
+$app->run();
