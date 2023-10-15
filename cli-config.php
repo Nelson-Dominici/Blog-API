d71ef1ed2e0e4dc68b374351b55056114e7d2ce7
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-15 16:37:14 -0300

    Updating bootstrap.php path.

--- cli-config.php
+++ cli-config.php
@@ -4,7 +4,7 @@ use app\Helpers\EntityManagerHelper;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
 
-require_once("bootstrap.php");
+require_once "public/bootstrap.php";
 
 $entityManager = EntityManagerHelper::getEntityManager();
 
