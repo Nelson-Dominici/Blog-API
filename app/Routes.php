3b5bc341b685b8293c85c3679f0108dde58faf4b
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-15 16:39:13 -0300

    Placing Auth routes.

--- app/Routes.php
+++ app/Routes.php
@@ -6,6 +6,7 @@ $app->group("/api", function(Group $group): void {
 
 	$group->group("/v1/users", function(Group $group): void {
 		require_once("Modules/User/UserRouter.php");
+		require_once("Modules/Auth/AuthRouter.php");
 	});
 
 	$group->group("/v1/posts", function(Group $group): void {
