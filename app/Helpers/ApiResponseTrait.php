819566eb213d62df6b99dd5df6ea94131e8f8ecd
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-15 16:41:06 -0300

    Using new way of send a response.

--- app/Helpers/ApiResponseTrait.php
+++ app/Helpers/ApiResponseTrait.php
@@ -6,29 +6,11 @@ use Slim\Psr7\Response;
 
 trait ApiResponseTrait
 {
-	protected function success(mixed $data = false, int $statusCode = 200): Response
+	protected function jsonResponse(array $data = [], int $statusCode = 200): Response
 	{
 		$res = new Response();
-
-		$data = !$data 
-			? ['success' => true]
-			: ['success' => true, 'data' => $data];
 
 		$res->getBody()->write(json_encode($data));
-
-		return $res
-				->withHeader("Content-Type", "application/json")
-				->withStatus($statusCode);
-	}
-
-	protected function error(string $message, int $statusCode = 400): Response
-	{
-		$res = new Response();
-
-		$res->getBody()->write(json_encode([
-			'success' => false,
-			'error' => $message
-		]));
 
 		return $res
 				->withHeader("Content-Type", "application/json")
