3c4135c52c16b8c41ebe25598bc5279b7411dcf5
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-16 01:29:36 -0300

    Using correct name.

---
+++ app/Modules/Post/Services/EditPostContentService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\Modules\Post\Services;
+
+use app\Entitys\{
+	Posts,
+	Users
+};
+
+use app\Helpers\{
+	AppException,
+	EntityManagerHelper
+};
+
+class EditPostContentService
+{
+	public static function handle(string $postUuid, array $reqBody): void
+	{
+		$entityManager = EntityManagerHelper::getEntityManager();
+		$usersRepository = $entityManager->getRepository(Users::class);
+
+		$user = $usersRepository->findOneBy([
+		    "uuid" => $userUuid
+		]);
+
+		if (!$user->getAdm()) {
+			throw new AppException("Unauthorized user.", 401);
+		}
+
+		$postRepository = $entityManager->getRepository(Posts::class);
+
+		$post = $postRepository->findOneBy([
+		    "uuid" => $postUuid
+		]);
+
+		if (!$post) {
+
+			throw new AppException("Post uuid not found", 404);
+		}
+
+	    $post->setContent($reqBody["newContent"]);
+	    $entityManager->flush();
+	}
+}
