5cdf6f4cda6c9712e58cbf2f38c9ceb99f2c9fe1
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-21 11:48:50 -0300

    Using new way of crete routes.

--- app/Routes.php
+++ app/Routes.php
@@ -1,17 +1,27 @@
 <?php
 
+use Slim\App;
+
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
-$app->group("/api", function(Group $group): void {
+return function($app) {
+	$app->group("/api", function(Group $group): void {
 
-	$group->group("/v1/users", function(Group $group): void {
-		require_once("Modules/User/UserRouter.php");
-		require_once("Modules/Auth/AuthRouter.php");
-	});
+		$group->group("/v1/users", function(Group $group): void {
+			$userRouter = require_once("Modules/User/UserRouter.php");
+			$authRouter = require_once("Modules/Auth/AuthRouter.php");
+			
+			$userRouter($group);
+			$authRouter($group);
+		});
 
-	$group->group("/v1/posts", function(Group $group): void {
-		require_once("Modules/Post/PostRouter.php");
-		require_once("Modules/Comment/CommentRouter.php");
-	}); 
+		$group->group("/v1/posts", function(Group $group): void {
+			$postRouter = require_once("Modules/Post/PostRouter.php");
+			$commentRouter = require_once("Modules/Comment/CommentRouter.php");
+		
+			$postRouter($group);
+			$commentRouter($group);
+		}); 
 
-});	
+	});	
+};
